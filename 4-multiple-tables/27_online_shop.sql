8d993086c00f81ebc9570e27b3682853669b0633
FARDIN SHARIAR HAQUE NEHAL <fardin.nehal.252@northsouth.edu>
2026-03-31 17:21:13 +0600

    Add SQL queries for online shop data retrieval
    Added SQL queries to retrieve order, product, and customer data, as well as calculate total sold items and daily revenue.

--- 4-multiple-tables/27_online_shop.sql
+++ 4-multiple-tables/27_online_shop.sql
@@ -1,4 +1,30 @@
 -- Online Shop 🛍️
 -- Codédex
 
--- No solution for this one yet! :)
+-- Write code below 💖
+
+select * from orders;
+select * from products;
+select * from customers;
+
+
+select p.name, COUNT(o.item_id) as total_sold
+from products as p
+JOIN orders as o 
+ON o.item_id = p.id
+GROUP BY p.name 
+ORDER BY total_sold DESC;
+
+
+SELECT c.name AS customer_name, p.name AS product_name, o.date
+FROM customers AS c
+JOIN orders AS o ON c.id = o.customer_id
+JOIN products AS p ON o.item_id = p.id
+ORDER BY c.name;
+
+
+SELECT o.date, SUM(p.price) AS daily_revenue
+FROM orders AS o
+JOIN products AS p ON o.item_id = p.id
+GROUP BY o.date
+ORDER BY daily_revenue DESC;
